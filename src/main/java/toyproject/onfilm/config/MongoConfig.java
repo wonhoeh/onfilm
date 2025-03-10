e05f7828bb90d84d44c9d2e29e8c437b0a09e08e
whheo <wonhoeh@gmail.com>
2025-03-10 16:56:40 +0900

    Deploy Test

--- src/main/java/toyproject/onfilm/config/MongoConfig.java
+++ src/main/java/toyproject/onfilm/config/MongoConfig.java
@@ -4,14 +4,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-//@Configuration
-//@EnableMongoRepositories(basePackages = "toyproject.onfilm.like.repository")
+@Profile("prod")
+@Configuration
+@EnableMongoRepositories(basePackages = "toyproject.onfilm.like.repository")
 public class MongoConfig {
 
-//    @Bean
+    @Bean
     public MongoTemplate mongoTemplate() {
         //MongoDB 연결 문자열을 사용하여 MongoClient를 생성합니다.
         MongoClient mongoClient = MongoClients.create("mongodb://onfilm.p-e.kr:27017");
