c832c7f3f8a0eee1a5935aa93bb5a5ad2a50f2ea
whheo <wonhoeh@gmail.com>
2025-03-14 20:50:56 +0900

    [TEST] RDS Connection

--- src/main/java/toyproject/onfilm/OnfilmApplication.java
+++ src/main/java/toyproject/onfilm/OnfilmApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-//@EnableMongoRepositories(basePackages = "toyproject.onfilm.like.repository", mongoTemplateRef = "mongoTemplate")
-@EnableMongoRepositories
+@EnableMongoRepositories(basePackages = "toyproject.onfilm.like.repository", mongoTemplateRef = "mongoTemplate")
+//@EnableMongoRepositories
 @SpringBootApplication
 public class OnfilmApplication {
 
