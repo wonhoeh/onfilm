949abc408d3f2a2a968f3c8f375a2ab106c4a7f7
whheo <wonhoeh@gmail.com>
2025-12-20 20:37:13 +0900

    refactor: User 필드 수정

--- src/main/java/com/onfilm/domain/genre/dto/GenreIdResponse.java
+++ src/main/java/com/onfilm/domain/genre/dto/GenreIdResponse.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class GenreIdResponse {
-
     private String genreId;
 
     public GenreIdResponse(String genreId) {
--- src/main/java/com/onfilm/domain/user/entity/User.java
+++ src/main/java/com/onfilm/domain/user/entity/User.java
@@ -8,25 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="user_id")
     private Long id;
 
-    private String webId;
-    private String password;
-    private String name;
-    private int age;
     private String email;
-
-    //=== 연관 관계 ===
-    //유저가 작성한 댓글 목록
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-//    List<Comment> comments = new ArrayList<>();
-
-    //유저가 누른 좋아요
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-//    List<Like> likes = new ArrayList<>();
+    private String password;
 }
