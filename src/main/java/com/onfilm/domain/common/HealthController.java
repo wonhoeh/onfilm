9a81933ef51ae516406c6ac987929dfea8f9b7c7
whheo <wonhoeh@gmail.com>
2026-02-21 19:17:23 +0900

    feat: ELB health check api 생성
    - GET /health 요청
    - 200 응답

---
+++ src/main/java/com/onfilm/domain/common/HealthController.java
@@ -0,0 +1,14 @@
+package com.onfilm.domain.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("health check");
+    }
+}
