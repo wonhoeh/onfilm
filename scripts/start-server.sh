8974214350702a865005f4b2f7e8990a815ab116
whheo <wonhoeh@gmail.com>
2026-03-10 06:09:21 +0900

    fix: start-server 스크립트 수정
    - 프로필 변수 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -14,6 +14,6 @@ set +a
 
 sudo fuser -k -n tcp 8080 || true
 
-nohup java -jar project.jar > ./output.log 2>&1 &
+nohup java -jar project.jar --spring.profiles.active=prod > ./output.log 2>&1 &
 
 echo "--------------- 서버 배포 끝 -----------------"
